5a4a4701aad0563fa6a6d3231c9ab0b444e8a9c3
Balaguru <balagurusiva2002@gmail.com>
2023-07-29 13:10:56 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-let otp
-otp = Math.floor(1000 + (10000 - 1000) * Math.random());
+function otp4(){ 
+    let otp = Math.floor(1000 + (10000 - 1000) * Math.random())
+    let sotp = otp.toString()
+    let otpArray = sotp.split("") 
 
-let sotp = otp.toString()
+    let nodeToshow=  document.querySelectorAll(".otp-4-box")
+    for (let i = 0; i < nodeToshow.length; i++) {
+     nodeToshow[i].innerHTML = otpArray[i]
+   } 
+
+   let nodeToHide=  document.querySelectorAll(".otp-6-box")
+   for (let i = 0; i < nodeToHide.length; i++) {
+    nodeToHide[i].setAttribute("style","display:none")
+}
+}
 
 
-console.log(sotp.split(""))
+function otp6(){
+    let otp = Math.floor(100000 + (10000000 - 1000) * Math.random())
+    let sotp = otp.toString()
+    let otpArray = sotp.split("") 
+
+    let nodeToshow=  document.querySelectorAll(".otp-box")
+    for (let i = 0; i < nodeToshow.length; i++) {
+     nodeToshow[i].innerHTML = otpArray[i]
+   }
+
+   let nodeToHide=  document.querySelectorAll(".otp-6-box")
+   for (let i = 0; i < nodeToHide.length; i++) {
+    nodeToHide[i].setAttribute("style","display:normal")
+}
+
+    
+}
+
+
+ 
